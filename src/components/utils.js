5e927dd58dfb6da65800ad8d480621d7357e6a9f
kyryloren <dev@kyryloorlov.com>
2022-04-04 11:01:10 -0400

    Fixed YouTube link

--- src/components/utils.js
+++ src/components/utils.js
@@ -2,7 +2,7 @@ export const SOCIALS = {
   INSTAGRAM: "https://instagram.com/techcodeshq",
   TWITTER: "https://twitter.com/techcodeshq",
   GITHUB: "https://github.com/techcodeshq",
-  YOUTUBE: "https://www.youtube.com/channel/UC74GHmtwQoj3bFxw2pBYM0Aq",
+  YOUTUBE: "https://www.youtube.com/channel/UC74GHmtwQoj3bFxw2pBYM0A",
   LINKEDIN: "https://www.linkedin.com/company/techcodes",
 };
 export function hexToRgbA(hex = "#FFFFFF", alpha) {
